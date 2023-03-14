361288b0e934f1f962aeab4dc1ce512b81c42b03
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-14 18:01:11 -0400

    Remove c++ reference code

--- source/app.d
+++ source/app.d
@@ -4,23 +4,6 @@ import bindbc.glfw;
 import erupted.functions;
 import window = window.window;
 
-/** C++ reference code
-
-class HelloTriangleApplication {
-    void run() {
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
-
-    void initVulkan() {}
-    void mainLoop() {}
-    void cleanup() {}
-};
-
-calls run(); in main();
-*/
-
 void main() {
     window.initialize();
 
