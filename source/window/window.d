9af28021873bac6c421e6cc7505b35a60f2ef3ff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-15 04:01:32 -0400

    Translated to D

--- source/window/window.d
+++ source/window/window.d
@@ -181,22 +181,13 @@ void executeHackJobShaderCompile() {
 
 
 
-static std::vector<char> readFile(const std::string& filename) {
-    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+char[] readFile(string fileLocation) {
 
-    if (!file.is_open()) {
-        throw std::runtime_error("failed to open file!");
+    if (!exists(fileLocation)) {
+        throw new Exception("Vulkan: File " ~ fileLocation ~ " does not exist!");
     }
 
-    size_t fileSize = (size_t) file.tellg();
-    std::vector<char> buffer(fileSize);
-
-    file.seekg(0);
-    file.read(buffer.data(), fileSize);
-
-    file.close();
-
-    return buffer;
+    return cast(char[])read(fileLocation);
 }
 
 
