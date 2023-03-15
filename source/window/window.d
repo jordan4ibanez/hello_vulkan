daee01c04822070c0800d425c4083b5fad049116
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-15 04:09:34 -0400

    Now finish this chapter

--- source/window/window.d
+++ source/window/window.d
@@ -219,9 +219,19 @@ void createGraphicsPipeline() {
     VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
     VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
 
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo;
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.Module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
 
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo;
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.Module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
 
-
+    VkPipelineShaderStageCreateInfo[] shaderStages = [vertShaderStageInfo, fragShaderStageInfo];
 
     // All of the remaining code in this chapter will be inserted before this line
     vkDestroyShaderModule(device, fragShaderModule, VK_NULL_HANDLE);
